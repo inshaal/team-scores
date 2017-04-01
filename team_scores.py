cfff1b6c4ac9f8024dbd3a3ee60d16a934fffcdb
Inshaal Uddin <inshaal@users.noreply.github.com>
2017-04-01 10:02:54 +0530

    updated code

--- team_scores.py
+++ team_scores.py
@@ -4,8 +4,8 @@ f={}
 g={}
 while True:
     a=raw_input("Enter the name of the team : ")
-    b=raw_input("Enter matches won : ")
-    c=raw_input ("Enter matches lost : ")
+    b=raw_input("Enter matches won by that team : ")
+    c=raw_input ("Enter matches lost by that team : ")
     d[a]=[b, c]
     f[a]=[b]
     g[a]=[c]
@@ -17,7 +17,7 @@ while True:
 print "The dictionary created is : "
 print d
 print "All the teams are : ", d.keys() 
-print "The Matches won by teams are : "
+print "Matches won by teams are : "
 print f
-print "All the matches lost by teams are : "
+print "Matches lost by teams are : "
 print g
